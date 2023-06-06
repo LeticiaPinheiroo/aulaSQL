c4d8f868fcba057b811726ab0de8f0edc098c856
LeticiaPinheiroo <126022547+LeticiaPinheiroo@users.noreply.github.com>
2023-06-06 08:13:34 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,33 +1,83 @@
-<?php
-$link = mysqli_connect("localhost:3306", "root", "", "projetoweb");
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Agendamentos</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f2f2f2;
+            margin: 0;
+            padding: 0;
+        }
 
-if (!$link){
-    echo "Error:  Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    exit;
-}
+        .container {
+            max-width: 800px;
+            margin: 20px auto;
+            background-color: #fff;
+            padding: 20px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-radius: 5px;
+        }
 
-echo "Sucesso: sucesso ao conectar-se com a base de dados MySQL." . PHP_EOL;
+        h1 {
+            color: #333;
+            text-align: center;
+        }
 
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
 
-$id = 1;
-$nome = 'Leticia Pinheiro';
-$endereco = 'Av. Aguia de Haia';
+        th, td {
+            padding: 10px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
 
-$sql = "INSERT INTO agendamentos (id, nome, endereco, bairro, cep) VALUES (?,?,?,?,?)";
-$link->prepare($sql)->execute([$id, $nome, $endereco, $bairro, $cep]);
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Agendamentos</h1>
 
-$stmt = $link->query("SELECT * FROM agendamentos");
-while($row = $stmt->fetch_assoc()){
-    echo $row['id']."<br />\n";
-    echo $row['nome']."<br />\n";
-    echo $row['endereco']."<br />\n";
-    echo $row['bairro']."<br />\n";
-    echo $row['cep']."<br />\n";
-   
-}
+        <?php
+        // Código PHP para recuperar e exibir os dados do banco de dados
+        $link = mysqli_connect("localhost:3306", "root", "", "projetoweb");
+        // Verificar a conexão
 
+        if (!$link){
+            echo "Error:  Falha ao conectar-se com o banco de dados MySQL." . PHP_EOL;
+            echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+            echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+            exit;
+        }
 
-mysqli_close($link);
-?>
+        $sql = "SELECT * FROM agendamentos";
+        $result = $link->query($sql);
+
+        if ($result->num_rows > 0) {
+            echo "<table>";
+            echo "<tr><th>ID</th><th>Nome</th><th>Endereço</th><th>Bairro</th><th>CEP</th></tr>";
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>".$row['id']."</td>";
+                echo "<td>".$row['nome']."</td>";
+                echo "<td>".$row['endereco']."</td>";
+                echo "<td>".$row['bairro']."</td>";
+                echo "<td>".$row['cep']."</td>";
+                echo "</tr>";
+            }
+            echo "</table>";
+        } else {
+            echo "Nenhum agendamento encontrado.";
+        }
+
+        mysqli_close($link);
+        ?>
+    </div>
+</body>
+</html>
